5d31844de80dd0c7aeebe560a3f9e4806d1e4d32
Evrat <Evratal@yandex.ru>
2025-06-28 21:15:38 +0300

    Homework 35.1 update№1

--- habits/apps.py
+++ habits/apps.py
@@ -5,6 +5,6 @@ class HabitsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'habits'
 
-    # Полностью отключаем ready() на время миграций
-    # def ready(self):
-    #     import habits.signals
+
+    def ready(self):
+         import habits.signals
